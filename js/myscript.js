7e310262e43b05fe0e98fe103dde73d90e2f9fe8
Kirill Dmitriev <bulletformyvalentine67.ru@mail.ru>
2016-08-11 14:12:55 +0300

    Fix

--- js/myscript.js
+++ js/myscript.js
@@ -142,77 +142,7 @@ function homeHeight(){
 	var wh = jQuery(window).height() - 80;
 	jQuery('.top_slider, .top_slider .slides li').css('height', wh);
 }
-
-
-/*-----------------------------------------------------------------------------------*/
-/*	OWLCAROUSEL
-/*-----------------------------------------------------------------------------------*/
-$(document).ready(function() {
 	
-	//WORKS SLIDER
-    var owl = $(".owl-demo.projects_slider");
-
-    owl.owlCarousel({
-		navigation: true,
-		pagination: false,
-		items : 4,
-		itemsDesktop : [1000,4],
-		itemsDesktop : [600,3]
-	});
-	
-	
-	//TEAM SLIDER
-    var owl = $(".owl-demo.team_slider");
-
-    owl.owlCarousel({
-		navigation: true,
-		pagination: false,
-		items : 3,
-		itemsDesktop : [600,2]
-	});
-	
-	
-	
-	jQuery('.owl-controls').addClass('container');
-	
-	
-	//TESTIMONIALS SLIDER
-    var owl = $(".owl-demo.testim_slider");
-
-    owl.owlCarousel({
-		itemsCustom : [
-			[0, 1]
-        ],
-		navigation: false,
-		pagination: true,
-		items : 1
-	});
-	
-	
-	
-	jQuery('.owl-controls').addClass('container');
-	
-	
-});
-
-
-/*-----------------------------------------------------------------------------------*/
-/*	IFRAME TRANSPARENT
-/*-----------------------------------------------------------------------------------*/
-jQuery(document).ready(function() {
-	$("iframe").each(function(){
-		var ifr_source = $(this).attr('src');
-		var wmode = "wmode=transparent";
-		if(ifr_source.indexOf('?') != -1) {
-		var getQString = ifr_source.split('?');
-		var oldString = getQString[1];
-		var newString = getQString[0];
-		$(this).attr('src',newString+'?'+wmode+'&'+oldString);
-		}
-		else $(this).attr('src',ifr_source+'?'+wmode);
-	});
-});
-
 
 /*-----------------------------------------------------------------------------------*/
 /*	FOOTER HEIGHT
